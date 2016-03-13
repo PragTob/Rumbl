c146dfadb144e4124b416969fde3a5102c790954
Tobias Pfeiffer <tobi@bitcrowd.net>
2016-03-13 15:23:16 +0100

    Video Channel and commenting works

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -12,15 +12,85 @@ let Video = {
   },
 
 onReady(videoId, socket) {
+    console.log("On Ready!")
     let msgContainer = document.getElementById("msg-container")
     let msgInput     = document.getElementById("msg-input")
     let postButton   = document.getElementById("msg-submit")
     let vidChannel   = socket.channel("videos:" + videoId)
 
-    vidChannel.join()
-      .receive("ok", (resp) => console.log("joined the video channel", resp))
-      .receive("error", (reason) => console.log("failed to join the channel", reason))
-  }
+    msgContainer.addEventListener("click", (event) => {
+      event.preventDefault()
+      let seconds = event.target.getAttribute("data-seek") ||
+                    event.target.parentNode.getAttribute("data-seek")
+      if(!seconds) { return }
 
+      Player.seekTo(seconds)
+    })
+
+    postButton.addEventListener("click", (event) => {
+      console.log("clickety click")
+      let payload = {body: msgInput.value, at: Player.getCurrentTime()}
+      vidChannel.push("new_annotation", payload)
+                .receive("error", (error) => console.log(error))
+      msgInput.value = ""
+    })
+
+    vidChannel.on("new_annotation", (response) => {
+      vidChannel.params.last_seen_id = response.id
+      this.renderAnnotation(msgContainer, response)
+    })
+
+    vidChannel.join()
+      .receive("ok", ({annotations}) => {
+        let ids = annotations.map((annotation) => annotation.id)
+        vidChannel.params.last_seen_id = Math.max(...ids)
+        this.scheduleMessages(msgContainer, annotations)
+      })
+      .receive("error", (reason) => console.log("failed to join the channel", reason))
+  },
+
+  renderAnnotation(msgContainer, {user, body, at}) {
+    let template = document.createElement("div")
+    template.innerHTML = `
+      <a href="#" data-seek="${this.escape(at)}">
+        [${this.formatTime(at)}]
+        <strong>${this.escape(user.username)}</strong>: ${this.escape(body)}
+      </a>
+      `
+    msgContainer.appendChild(template)
+    msgContainer.scrollTop = msgContainer.scrollHeight
+  },
+
+  scheduleMessages(msgContainer, annotations) {
+    setTimeout(() => {
+      let ctime = Player.getCurrentTime()
+      let remaining = this.renderAtTime(annotations, ctime, msgContainer)
+      this.scheduleMessages(msgContainer, remaining)
+    }, 1000)
+  },
+
+  renderAtTime(annotations, seconds, msgContainer) {
+    return annotations.filter ( (annotation) => {
+      // TODO: untidy... filters and modifies in one loop,
+      if (annotation.at > seconds) {
+        return true
+      } else {
+        this.renderAnnotation(msgContainer, annotation)
+        return false
+      }
+    })
+  },
+
+  formatTime(at) {
+    let date = new Date(null)
+    date.setSeconds(at / 1000)
+    return date.toISOString().substr(14, 5)
+  },
+
+  escape(string) {
+    let div = document.createElement("div")
+    div.appendChild(document.createTextNode(string))
+    return div.innerHTML
+  }
 }
 export default Video
